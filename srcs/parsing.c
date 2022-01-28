10e98c59688277fb843909de9b5108e2e2984653
Chi-khang DUONG <chduong@e2r3p12.clusters.42paris.fr>
2022-01-28 19:24:38 +0100

    path join

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: chduong <chduong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 20:17:23 by chduong           #+#    #+#             */
-/*   Updated: 2022/01/28 17:42:57 by chduong          ###   ########.fr       */
+/*   Updated: 2022/01/28 18:19:53 by chduong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,31 @@
 ◦ env with no options or arguments
 ◦ exit with no options*/
 
+char	*path_join(char *path, char *cmd)
+{
+	char	*p;
+	int		len1;
+	int		len2;
+
+	if (!path || !cmd)
+		return (NULL);
+	len1 = ft_strlen(path);
+	len2 = ft_strlen(cmd);
+	p = malloc(sizeof(char) * (len1 + len2 + 2));
+	if (!p)
+		return (NULL);
+	if (s1 && !s2)
+		ft_strlcpy(p, s1, len1 + 1);
+	if (!s1 && s2)
+		ft_strlcpy(p, s2, len2 + 1);
+	else
+	{
+		ft_strlcpy(p, s1, len1 + 1);
+		ft_strlcat(p, s2, len1 + len2 + 1);
+	}
+	return (p);
+}
+
 int	exec_cmd(char **arg, char *path)
 {
 	char	**paths;
@@ -30,7 +55,9 @@ int	exec_cmd(char **arg, char *path)
 	paths = ft_split(path, ':');
 	i = 0;
 	while (paths[i])
-		access(paths[i], )
+	{
+		if(access(paths[i], X_OK))
+	}
 }
 
 int	parse_line(const char *line, char *path)
