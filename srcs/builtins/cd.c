c4862e5c972ba2c11b00b2cae5bb00539b8fff19
Kenny DUONG <kennyduong@DK-MacBook.local>
2022-03-30 11:28:47 +0200

    cd brouillon

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,13 +6,22 @@
 /*   By: kennyduong <kennyduong@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 16:51:15 by kennyduong        #+#    #+#             */
-/*   Updated: 2022/03/29 20:34:31 by kennyduong       ###   ########.fr       */
+/*   Updated: 2022/03/30 11:15:36 by kennyduong       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    cd()
+void    cd(char *path, t_data *data)
 {
-    
+    if (ft_strncmp(path, "./", 2) == 0)
+    {
+        if ()
+    }
+    else if (ft_strncmp(path, "../", 2) == 0)
+    {
+        
+    }
+    else
+        chdir(path);
 }
